39ef0e05789965db994dae34da96468ad25a0b0d
kucha <2320818066@qq.com>
2025-01-03 23:58:34 +0800

    fix: 支持http协议图片

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,15 @@ const nextConfig: NextConfig = {
   output: 'standalone', // 启用独立输出模式
   env: {
     NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL
+  },
+  images: {
+    domains: ['image.xukucha.cn'],
+    remotePatterns: [
+      {
+        protocol: 'http',
+        hostname: 'image.xukucha.cn'
+      },
+    ],
   }
 };
 
